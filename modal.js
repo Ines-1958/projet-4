f8a3460a3a24da2816982ac11582bada5316fe6c
Ines-1958 <79308453+Ines-1958@users.noreply.github.com>
2021-07-02 11:24:22 +0200

    Commit initial

--- modal.js
+++ modal.js
@@ -11,13 +11,23 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close")//déclaration variable modalClose pour fermer la modale
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//appel de la fonction close modal
+modalClose.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+//close modal(création de la fonction closeModal pour fermer la modale)
+function closeModal(){
+  modalbg.style.display = "none";
+}
+
 
 
